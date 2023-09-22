62e8c8686976e02ae1102b39524d4764b9dee734
Michael Mancini <michael.mancini@covisiangroup.local>
2023-09-22 14:36:48 +0200

    Moved app initializer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import axios from 'axios'
+import {
+  GRANT_TYPE,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  USERNAME,
+  PASSWORD,
+  OAUTH_ENDPOINT
+} from '@/constants'
+import { store } from '@/store'
+import type { OauthApiResponse } from '@/types'
+import { onMounted } from 'vue'
+
+const initializeApp = async () => {
+  const formData = new FormData()
+
+  formData.append('grant_type', GRANT_TYPE)
+  formData.append('client_id', CLIENT_ID)
+  formData.append('client_secret', CLIENT_SECRET)
+  formData.append('username', USERNAME)
+  formData.append('password', PASSWORD)
+
+  await axios
+    .post<OauthApiResponse>(OAUTH_ENDPOINT, formData)
+    .then((res) => res.data)
+    .then((res) => (store.bearer = `${res.token_type} ${res.access_token}`))
+    .catch(console.error)
+}
+
+onMounted(() => {
+  initializeApp()
+})
+</script>
+
 <template>
   <main>
     <router-view />
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,14 @@
 import App from '@/App.vue'
 import '@/assets/base.css'
-import {
-  CLIENT_ID,
-  CLIENT_SECRET,
-  GRANT_TYPE,
-  OAUTH_ENDPOINT,
-  PASSWORD,
-  USERNAME
-} from '@/constants'
-import { store } from '@/store'
-import type { OauthApiResponse } from '@/types'
 import { setBasePath } from '@shoelace-style/shoelace'
 import '@shoelace-style/shoelace/dist/themes/light.css'
-import axios from 'axios'
 import { createApp } from 'vue'
 import router from './router'
 
-const initializeApp = async () => {
-  const formData = new FormData()
+setBasePath('/node_modules/@shoelace-style/shoelace/cdn')
 
-  formData.append('grant_type', GRANT_TYPE)
-  formData.append('client_id', CLIENT_ID)
-  formData.append('client_secret', CLIENT_SECRET)
-  formData.append('username', USERNAME)
-  formData.append('password', PASSWORD)
+const app = createApp(App)
 
-  await axios
-    .post<OauthApiResponse>(OAUTH_ENDPOINT, formData)
-    .then((res) => res.data)
-    .then((res) => (store.bearer = `${res.token_type} ${res.access_token}`))
-    .catch(console.error)
-}
+app.use(router)
 
-await initializeApp().then(() => {
-  setBasePath('/node_modules/@shoelace-style/shoelace/cdn')
-
-  const app = createApp(App)
-
-  app.use(router)
-
-  app.mount('#app')
-})
+app.mount('#app')
